07fd19e67603d8841ba17405bb28227b01fd2dbf
sondonghup <sondonghup@naver.com>
2023-03-26 22:54:35 +0900

    reshape bot.py

--- apis/models/chat.py
+++ apis/models/chat.py
@@ -1,33 +1,35 @@
-import torch
+import os
 from transformers import (
     PreTrainedTokenizerFast,
     GPT2LMHeadModel
 )
 
 def load_model(model_name):
-    token = '입력'
     tokenizer = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
                                                         bos_token='<s>',
                                                         eos_token='</s>', 
                                                         unk_token='<unk>', 
                                                         pad_token='<pad>', 
-                                                        mask_token='<unused0>')
-    model = GPT2LMHeadModel.from_pretrained(model_name, use_auth_token = token).eval()
+                                                        mask_token='<unused0>'
+                                                        )
+    model = GPT2LMHeadModel.from_pretrained(model_name, use_auth_token = True).eval()
     return tokenizer, model
 
 def predict(inputs, tokenizer, model):
     encoded_inputs = tokenizer.encode(inputs, return_tensors="pt")
     outputs = model.generate(encoded_inputs,
                                 max_length=64,
+                                min_length = 10,
                                 pad_token_id=tokenizer.pad_token_id,
                                 eos_token_id=tokenizer.eos_token_id,
                                 bos_token_id=tokenizer.bos_token_id,
                                 use_cache=True,
+                                do_sample = True,
                                 no_repeat_ngram_size = 3,
-                                temperature=0.9,
+                                temperature=1.05,
                                 top_k=50,
                                 top_p=0.92
                                 )
     
-    generated = tokenizer.decode(outputs[0])
+    generated = tokenizer.decode(outputs[0], skip_special_tokens = True)
     return generated
--- bot.py
+++ bot.py
@@ -9,17 +9,24 @@ import time
 from discord import Embed
 from melonapi import scrapeMelon
 import json
+import os
 
-TOKEN = '입력'
+TOKEN = os.environ['discord_token']
 api_service_name = "youtube"
 api_version = "v3"
-DEVELOPER_KEY = '입력'
+DEVELOPER_KEY = os.environ['youtube_api_key']
 youtube = googleapiclient.discovery.build(
     api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
 load_dotenv()
 bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
 
+class youtube_quiz:
+    def __init__(self, quizUIFrame, voice, owner) -> None:
+        self._quizUIFrame = quizUIFrame
+        self._voice = voice
+        self._owner = owner
+        
 @bot.event
 async def on_ready():
     print(f'Login bot: {bot.user}')
@@ -130,8 +137,13 @@ async def game(ctx, *, player):
 
 async def countdown(isLong): # 카운트 다운
     leftsec = 40
-
-
+    limit = 0
+    while True:
+        if limit > 100: return
+        leftsec -= 1
+        if leftsec < 0: leftsec = 0
+        quizUIFrame._quizLeftTime = leftSec
+        await quizUIFrame.update()
 
 
 
@@ -169,105 +181,9 @@ async def on_ready(ctx):
 
     await ctx.send(embed=embed)
 
-
-
-
-
-@ bot.command()
-@ commands.cooldown(1, 4, commands.BucketType.user)
-async def info(ctx, *args):
-    output2 = ''
-    print(f"args : {args}")
-    for word in args:
-        output2 += str(word)
-        output2 += ' '
-        print(output2)
-    request3 = youtube.search().list(
-        q = f"{output2}",
-        order = 'rating',
-        part="snippet",
-        maxResults = 1
-    )
-    response3 = request3.execute()
-    print(response3)
-    channame = response3['items'][0]['snippet']['title']
-    chanid = response3['etag']
-    dsc = response3['items'][0]['snippet']['description']
-    embed2 = discord.Embed(title=f"{channame}", url=f"https://www.youtube.com/watch?v={chanid}",
-                          description=f"{dsc}", color=0xFF5733)
-    # response3 = request3.execute()
-    # embed2.add_field(text=f"Made By Riceblades11")
-    await ctx.send(embed=embed2)
-
-
-@ bot.command()
-@ commands.cooldown(1, 30, commands.BucketType.user)
-async def search(ctx, *args):
-    output = ''
-    for word in args:
-        output += str(word)
-        output += ' '
-    request = youtube.search().list(
-        part="id,snippet",
-        type='video',
-        q=f"{output}",
-        videoDuration='short',
-        videoDefinition='high',
-        maxResults=1,
-        fields="nextPageToken,items(id(videoId),snippet(publishedAt,channelId,channelTitle,title,description))"
-    )
-    response = request.execute()
-    print(response)
-    await ctx.send(response)
-    chan = response['items'][0]['snippet']['channelId']
-    title = response['items'][0]['snippet']['title']
-    idd = response['items'][0]['id']['videoId']
-    dsc = response['items'][0]['snippet']['description']
-    embed = discord.Embed(title=f"{title}", url=f"https://www.youtube.com/watch?v={idd}",
-                          description=f"{dsc}", color=0xFF5733)
-    # embed.set_image(
-    #     url=f"https://i.ytimg.com/vi/{idd}/mqdefault.jpg")
-    request2 = youtube.channels().list(
-        part="snippet",
-        id=f"{chan}",
-    )
-    response2 = request2.execute()
-    pfp = response2['items'][0]['snippet']['thumbnails']['default']['url']
-    name = response2['items'][0]['snippet']['localized']['title']
-    embed.set_author(
-        name=name, url=f"https://www.youtube.com/channel/{chan}", icon_url=pfp)
-    # embed.set_footer(text="Made By Riceblades11")
-    view = DropdownView(idd)
-    await ctx.send(embed=embed, view=view)
-
-
-class Dropdown(discord.ui.Select):
-    def __init__(self, idd):
-        self.idd = idd
-        options = [
-            discord.SelectOption(
-                label='Link', description='Click for YouTube Link', emoji='🟥'),
-        ]
-        super().__init__(placeholder='Pick Option',
-                         min_values=1, max_values=1, options=options)
-
-    async def callback(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f'Link: https://www.youtube.com/watch?v={self.idd}', ephemeral=True)
-
-class DropdownView(discord.ui.View):
-    def __init__(self, idd):
-        super().__init__()
-        self.add_item(Dropdown(idd))
-
-
-
 @ bot.command()
 async def quit(ctx, player):
     channel = ctx.message.author.voice.channel
     await channel.disconnect()
-
-
-
-
 
 bot.run(TOKEN)
